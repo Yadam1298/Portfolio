37262ec6f7d56961c6f00cf01ba6aefee8759906
Yadam1298 <y.n.v.n.kumar@gmail.com>
2026-03-20 22:11:13 +0530

    updated home

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -44,7 +44,11 @@ const Home = () => {
   if (!data)
     return (
       <div className="home-container">
-        <p>Render server cold start in progress. Please refresh page in 120 seconds.</p>
+        <p>
+          Render server cold start in progress.
+          <br />
+          <span className="blinker">Please refresh page in 120 seconds.</span>
+        </p>
       </div>
     );
 
