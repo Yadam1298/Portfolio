64acdc06ff3746870d589bde7bfef0198b16d7b7
Yadam1298 <y.n.v.n.kumar@gmail.com>
2026-05-04 21:31:54 +0530

    initial commit

--- app/sitemap.js
+++ app/sitemap.js
@@ -4,5 +4,25 @@ export default function sitemap() {
       url: 'https://yadam.vercel.app',
       lastModified: new Date(),
     },
+    {
+      url: 'https://yadam.vercel.app/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://yadam.vercel.app/testimonials',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://yadam.vercel.app/projects',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://yadam.vercel.app/certifications',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://yadam.vercel.app/contact',
+      lastModified: new Date(),
+    },
   ];
 }
